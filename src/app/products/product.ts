3b188b993a72edc1101217d0deb77f394546e422
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-14 13:27:23 +0530

    Product interface

---
+++ src/app/products/product.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: Date,
+  description: string;
+  price: number;
+  starRating: number;
+  imageUrl: string;
+  caculateDiscount(percent: number): number;
+
+}
