4396d9b86909e18a666728643751633154d82314
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-16 23:18:42 +0530

    Error handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
-import { HttpClient} from '@angular/common/http';
-import { Observable} from 'rxjs';
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError} from 'rxjs';
+import { catchError , tap} from "rxjs/operators";
+import { ThrowStmt } from "@angular/compiler";
 
 @Injectable({
   providedIn:'root'
@@ -14,7 +16,23 @@ export class ProductService {
   }
 
   getProduct(): Observable<IProduct[]>{
-    return this.http.get<IProduct[]>(this.productUrl);
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('data --'+ JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(err: HttpErrorResponse){
+    let errorMessage = '';
+
+    if(err.error instanceof ErrorEvent){
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else{
+      errorMessage = `Server returned error code: ${err.status}, error message is ${err.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+
   }
 
 }
