1089b7ad80d3a02c982b32d2103eb96f7c6a5d12
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-20 18:01:25 +0530

    CanActivate Guard

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from "@angular/router";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
@@ -7,10 +7,20 @@ import { Observable } from "rxjs";
 })
 export class ProductDetailGuard implements CanActivate{
 
+  constructor(private router: Router){}
+
   canActivate(next: ActivatedRouteSnapshot,
   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree{
 
-    return true;
+    let id = +next.url[1].path;
+
+    if(isNaN(id) && id < 1){
+      console.log('Invalid product Id');
+      this.router.navigate(['\products']);
+      return false;
+    }; 
+      return true;
+    
 
   }
 
