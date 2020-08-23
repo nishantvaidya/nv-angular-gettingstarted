6d23205b5b6ce76a38698c7b5b02e0a7cb5473a2
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-23 15:35:34 +0530

    Product Module completion

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,31 @@
 import { NgModule} from '@angular/core';
 import  { CommonModule} from '@angular/common';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { StarComponent } from '../shared/star.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
 
 @NgModule({
-  declarations:[],
+  declarations:[
+    ProductDetailComponent,
+    ProductListComponent,
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
   imports :[
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent},
+      { 
+        path: 'products/:id', 
+        component: ProductDetailComponent,
+        canActivate: [ProductDetailGuard ]
+        },
+    ])
   ]
 })
 
