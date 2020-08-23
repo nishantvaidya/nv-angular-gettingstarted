d2bc55a37b2184fcb59113e76321ed579520baf6
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-23 15:53:08 +0530

    Product Module refactoring

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,7 @@
 import { NgModule} from '@angular/core';
-import  { CommonModule} from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
-import { StarComponent } from '../shared/star.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
