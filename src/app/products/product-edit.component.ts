336ddb06dc777889f1c509b9dfe2cade6541432f
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-31 17:54:54 +0530

    Generic validator

--- src/app/products/product-edit.component.ts
+++ src/app/products/product-edit.component.ts
@@ -1,12 +1,34 @@
-import { OnInit, Component } from "@angular/core";
+import { OnInit, Component, ViewChildren, AfterViewInit, OnDestroy, ElementRef } from "@angular/core";
+import { FormGroup, FormControlName } from "@angular/forms";
+import { IProduct} from './product'
+import { Subscription } from "rxjs";
 
 Component({
   templateUrl:'./product-edit.component.html'
 })
 
-export class ProductEditComponent implements OnInit{
+export class ProductEditComponent implements OnInit, AfterViewInit, OnDestroy{
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
+  pageTitle = 'Product Edit';
+  errorMessage:string;
+  productForm: FormGroup
+  product: IProduct;
+  private sub: Subscription;
+  displayMessage:{[key: string]: string} = {};
+  private validationMessage:{[key: string]: {[key: string]: string}}
+
+
 
   ngOnInit(){
+
+  }
+
+  ngOnDestroy( ){
+
+  }
+
+  ngAfterViewInit(){
 
   }
 }
