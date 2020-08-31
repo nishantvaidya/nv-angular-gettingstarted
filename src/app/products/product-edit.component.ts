2a4e99c41d960a6bfdbecad6c84d5866363d4b39
nishantvaidya <nishant.mca2005@gmail.com>
2020-08-31 16:54:12 +0530

    Product Edit Component HTML

---
+++ src/app/products/product-edit.component.ts
@@ -0,0 +1,12 @@
+import { OnInit, Component } from "@angular/core";
+
+Component({
+  templateUrl:'./product-edit.component.html'
+})
+
+export class ProductEditComponent implements OnInit{
+
+  ngOnInit(){
+
+  }
+}
