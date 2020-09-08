9d32e76b802bef34431fb0467680769fa316ad0a
nishantvaidya <nishant.mca2005@gmail.com>
2020-09-08 14:53:31 +0530

    User Module and LoginComponent

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { Router, RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+
+    ])
+  ],
+
+  declarations:[
+  
+  ]
+
+})
+
+export class UserModule{
+
+}
