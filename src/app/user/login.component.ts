0f5c46afc6ad9c5fe105497c7ec224f9264a0268
nishantvaidya <nishant.mca2005@gmail.com>
2020-09-10 14:03:12 +0530

    Login Component

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,9 +1,34 @@
 import { Component } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
   templateUrl:'./login.component.html'
 })
 export class LoginComponent {
+  errorMessage: string;
+  pageTitle: 'Log In';
+
+  constructor(private authService: AuthService,private router: Router){}
+
+  login(loginForm: NgForm): void {
+
+    if(loginForm && loginForm.valid){
+      const userName = loginForm.form.value.userName;
+      const password = loginForm.form.value.password;
+      this.authService.login(userName, password);
+      if(this.authService.redirectUrl){
+        this.router.navigateByUrl(this.authService.redirectUrl);
+      }else {
+        this.router.navigate(['/products']);
+      }
+
+    }else{
+        this.errorMessage ='Please enter a user name and password';
+    }
+
+  }
   
 
 }
