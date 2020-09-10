cc10fa7b5c4cf18acf1836fab75a749bca980ba8
nishantvaidya <nishant.mca2005@gmail.com>
2020-09-10 12:13:03 +0530

    User Module Configuration

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { Router, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-
+      { path : 'login', component: LoginComponent }
     ])
   ],
 
   declarations:[
+    LoginComponent
   
   ]
 
