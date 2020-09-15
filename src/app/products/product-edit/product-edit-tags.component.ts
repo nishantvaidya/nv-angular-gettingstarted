3b06c9d6e53d49733023935452a43e3a2c0b5de7
nishantvaidya <nishant.mca2005@gmail.com>
2020-09-15 17:39:09 +0530

    Product edit component

---
+++ src/app/products/product-edit/product-edit-tags.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: './product-edit-tags.component'
+})
+export class ProductEditTagsComponent {
+  
+}
---
+++ src/app/products/product-edit/product-edit.info.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRouteSnapshot, ActivatedRoute } from "@angular/router";
+import { IProduct } from "../product";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  templateUrl: './product-edit-info.component'
+})
+export class ProductEditInfoComponent implements OnInit {
+
+   @ViewChild(NgForm) productForm: NgForm;
+
+   errorMessage: string;
+   product: IProduct;
+
+  constructor(private route: ActivatedRoute){}
+  
+  ngOnInit(){
+
+    this.route.parent.data.subscribe(data =>{
+      if(this.productForm){
+        this.productForm.reset();
+      }
+      this.productForm = data['resolvedData'].product;
+
+    });
+
+
+  }
+
+}
